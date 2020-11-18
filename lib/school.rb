f630989ad68dbcf4cbbb234459d25633c0a32973
Jakob Persons <jakobep24@gmail.com>
2020-11-18 08:29:12 -0500

    building School class, more to be done

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+require 'pry'
+
+class School
+    def initialize(school)
+        @school = school
+    end
+
+    def roster
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @name = name
+        @grade = grade
+        @roster["@grade"] = []
+        @roster["@grade"] << @name
+        
+        
+        binding.pry
+    end
+
+end
