d16d00576d554757ef16e10b8ba52951dfd1b39b
Jakob Persons <jakobep24@gmail.com>
2020-12-05 17:38:48 -0500

    built school class

--- lib/school.rb
+++ lib/school.rb
@@ -2,22 +2,33 @@
 require 'pry'
 
 class School
-    def initialize(school)
-        @school = school
-    end
 
-    def roster
+    attr_accessor :name, :roster
+
+    def initialize(name)
+        @name = name
         @roster = {}
     end
 
-    def add_student(name, grade)
-        @name = name
-        @grade = grade
-        @roster["@grade"] = []
-        @roster["@grade"] << @name
-        
-        
-        binding.pry
+    # takes argument of student name and grade
+    # adds student to roster
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name
+    end
+
+    # returns hash of student grade and returns array of all students names in that grade
+    def grade(student_grade)
+        roster[student_grade]
+    end
+
+    # returns sorted array of students in each grade by student names
+    def sort
+        sorted = {}
+        roster.each do |grade, students|
+            sorted[grade] = students.sort
+        end
+        sorted
     end
 
 end
